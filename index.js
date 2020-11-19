ec612b0c47b84db8836bd5113001e1ecf094f8a3
Róisín Grannell <r.grannell2@gmail.com>
2020-11-19 21:26:02 +0000

    added favicon

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 
+const onPageLoad = () => {
+  const urlParams = new URLSearchParams(window.location.search)
+  const content = urlParams.get('content')
+
+  const $content = document.querySelector('#content')
+
+  if (content) {
+    $content.value = decodeURIComponent(content) || ''
+  }
+}
+
 const onContentChange = () => {
   const $content = document.querySelector('#content')
   const text = encodeURIComponent($content.value)
