5d63a3e6f81cee39d2045e35e43f19c8e89d1e27
R*Grannell <rgrannell1@users.noreply.github.com>
2023-10-13 20:27:39 +0100

    Added data-url output

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 
 // load from a url on pageload
-const onPageLoad = () => {
+function onPageLoad() {
   const urlParams = new URLSearchParams(window.location.search)
   const content = urlParams.get('content')
 
   const $content = document.querySelector('#content')
+  const $dataUrl = document.querySelector('#data-url')
 
   if (content) {
-    $content.value = decodeURIComponent(content) || ''
+    $content.value = decodeURIComponent(content) || '';
+    $dataUrl.value = `data:text/plain;charset=utf-8,${btoa(decodeURIComponent(content))}`;
   }
 }
 
 // update the url on change
-const onContentChange = () => {
+function onContentChange() {
   const $content = document.querySelector('#content')
+  const $dataUrl = document.querySelector('#data-url')
+
   const text = encodeURIComponent($content.value)
+
+  $dataUrl.value = `data:text/plain;charset=utf-8,${btoa(decodeURIComponent($content.value))}`;
 
   if (text) {
     const pageUrl = `?content=${text}`
